4a4da0d998ad8983d8a62f2a0b5f766af629720e
seg0ga <demin_s22@mail.ru>
2025-03-03 16:13:50 +0700

    Новый файл

---
+++ lab3/sort.c
@@ -0,0 +1,10 @@
+void SelectSort(struct Student students[N]){
+    for (int i=0;i<N;i++){
+        int k = i;
+        for (int j=i+1;j<N;j++){
+            if (students[k].sum < students[j].sum){
+                k=j;}}
+    struct Student stud=students[i];
+    students[i]=students[k];
+    students[k]=stud;}
+}
