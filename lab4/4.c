215b5da153ee8b69e741564f82e4e54f89fdd55c
Segoga <129649003+seg0ga@users.noreply.github.com>
2025-03-24 14:46:32 +0700

    Update 4.c

--- lab4/4.c
+++ lab4/4.c
@@ -3,22 +3,25 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
+
     if (argc!=2) {
-        perror("Не указаны все аргументы");
+        printf("Не указаны все аргументы");
         return 1;}
         
-    FILE *output_file=fopen(argv[1],"w");
-    if (!output_file){
-        perror("Ошибка открытия файла");
+    FILE *file=fopen(argv[1],"w");
+    if (file==NULL){
+        printf("Ошибка открытия файла");
         return 1;}
 
-    fprintf(output_file, "  x  | sin(x) | tan(x)\n");
-    fprintf(output_file, "----------------------\n");
+    fprintf(file, "  x  | sin(x) | tan(x)\n");
+    fprintf(file, "_____|________|________\n");
     double x,y,z;
     for (x=0;x<=3.1;x+=0.1) {
         y=sin(x);
         z=tan(x);
-        fprintf(output_file," %2.1f | %5.2f  | %5.3f\n", x, y, z);
-    }
-    fclose(output_file);
+        fprintf(file," %.1f |  %.2f  | %.3f\n", x, y, z);}
+        
+    if (fclose(file)!=0){
+        printf("Ошибка закрытия файла");
+        return 1;}
 }
