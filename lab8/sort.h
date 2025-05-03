069057611b372b84d8162cb5725bdaa0e3688bce
Segoga <129649003+seg0ga@users.noreply.github.com>
2025-05-03 13:51:28 +0700

    Create sort.h

---
+++ lab8/sort.h
@@ -0,0 +1,4 @@
+void generateKnuthSequence(int n,int sequence[],int *size);
+void shellSort(int arr[],int n,int *M,int *C);
+void insertionSort(int arr[],int n,int *M,int *C);
+void generateArray(int arr[],int n);
